fca85aa874e9fb7e699612afab00ebd4e5619ce7
Klimentij <klimentijkvlvsk@gmail.com>
2026-09-17 14:46:12 +0300

    Converted to Matkdown whole files with notes

--- src/config.py
+++ src/config.py
@@ -7,5 +7,6 @@ KNOWLEDGE_FILE = "data/processed/fil_ag.jsonl"
 QDRANT_URL = "http://localhost:6333"
 COLLECTION_NAME = "math_notes"
 EMBEDDING_MODEL = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+
 LLM_MODEL = "qwen2.5:7b"
 TOP_K = 5
